f63bfad41345060b22bf1bc7140d1bc90252d429
unknown <seablue12359@gmail.com>
2024-12-12 22:53:01 +0530

    Added spring setup

---
+++ oneToOneChat/src/main/java/com/chat/oneToOneChat/OneToOneChatApplication.java
@@ -0,0 +1,13 @@
+package com.chat.oneToOneChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOneChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToOneChatApplication.class, args);
+	}
+
+}
